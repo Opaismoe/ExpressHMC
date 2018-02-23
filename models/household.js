05fb7c48498875adfd7a51007f1f11ca96e843bf
opaismoe <wiebe.buursink@gmail.com>
2018-02-23 15:52:07 +0100

    Add householdPot to household model

--- models/household.js
+++ models/household.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose
 
 const householdSchema = new Schema({
   householdCount: { type: Number, default: 0 },
+  householdPot: { type: Number, default: 0 },
   week: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
