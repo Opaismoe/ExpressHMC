c53e238f397d211f4209d935972183a90c42c02f
Wiebe Buursink <wiebe.buursink@gmail.com>
2018-03-04 22:33:23 +0100

    Error handeling for Sign in

--- routes/sessions.js
+++ routes/sessions.js
@@ -4,10 +4,20 @@ const passport = require('../config/auth')
 const { User } = require('../models')
 const jwtOptions = require('../config/jwt')
 
-router.post('/sessions', passport.authenticate('local'), (req, res) => {
-  const payload = { id: req.user.id }
-  const token = jwt.sign(payload, jwtOptions.secretOrKey)
-  res.json({ token })
-})
+router.post('/sessions', function(req, res, next) {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      const error = new Error(info.message)
+      return next(error);
+    }
+
+    const payload = { id: user._id }
+    const token = jwt.sign(payload, jwtOptions.secretOrKey)
+    return res.json({ token })
+  })(req, res, next);
+});
 
 module.exports = router
