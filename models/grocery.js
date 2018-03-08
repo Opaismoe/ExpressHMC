6ad0eb0b093a804448ee1cff5b51dc285067bb76
Wiebe Buursink <wiebe.buursink@gmail.com>
2018-03-08 13:13:03 +0100

    Added pot, and household to grocery model.

--- models/grocery.js
+++ models/grocery.js
@@ -1,6 +1,18 @@
 const mongoose = require('../config/database')
 const { Schema } = mongoose
 
+const potSchema = new Schema({
+  totalAmount: { type: Number, default: 0 },
+})
+
+const householdSchema = new Schema({
+  householdCount: { type: Number, default: 0 },
+  householdPot: { type: Number, default: 0 },
+  week: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
 const grocerySchema = new Schema({
   text: { type: String, default: '' },
   price: { type: Number, default: 0 },
@@ -8,6 +20,9 @@ const grocerySchema = new Schema({
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   user: { type: Schema.Types.ObjectId, ref: 'users'},
+  pot: [potSchema],
+  household: [householdSchema],
 });
+
 
 module.exports = mongoose.model('grocerys', grocerySchema)
